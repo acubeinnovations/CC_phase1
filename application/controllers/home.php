56067f48daa98f93b7beaaf5f21b80bdbc8d79aa
Nijo Joseph <nijojoseph@acube.co>
2014-08-14 11:55:26 +0530

    comments added in home

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -4,18 +4,6 @@ class Home extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -  
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in 
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
 	 */
 	public function index($user = 'Accube Innovations')
 	{	$this->load->helper('url');
@@ -27,5 +15,5 @@ class Home extends CI_Controller {
 	}
 }
 
-/* End of file welcome.php */
-/* Location: ./application/controllers/welcome.php */
+/* End of file home.php */
+/* Location: ./application/controllers/home.php */
