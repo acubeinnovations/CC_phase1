ba9055a0a55c0f0987f63377cc94b00db0ab645c
Nijo Joseph <nijojoseph@acube.co>
2014-08-20 19:34:00 +0530

    captch afor secure login

---
+++ application/controllers/captcha.php
@@ -0,0 +1,10 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Captcha extends CI_Controller {
+function index(){
+
+$code = $this->captchaimage->generateCaptcha();
+$this->session->set_userdata(array('captcha_code'=> $code));
+}
+}
+?>
