dc41a37bab144bf230342ef29c3f3af85a3976c0
Nijo Joseph <nijojoseph@acube.co>
2014-09-01 10:38:30 +0530

    htaccess and trip booking

--- application/views/user-pages/trip-booking.php
+++ application/views/user-pages/trip-booking.php
@@ -9,6 +9,33 @@
 						<div class="form-group">
 						<?php $class="form-control row-source-50-percent-width-with-margin-8";
 						echo $this->form_functions->populate_dropdown('status',$user_status='',$selected='',$class,$msg="Select Source");
+						echo form_input(array('name'=>'source','class'=>'form-control row-source-50-percent-width-with-margin-8','id'=>'source','placeholder'=>'Source','value'=>'')); ?>
+						</div>
+					</fieldset>
+				</div>
+				<div class="booking-source">
+					<fieldset class="body-border">
+					<legend class="body-head">Passenger Informations</legend>
+						<div class="form-group">
+						<?php $class="form-control row-source-50-percent-width-with-margin-8";
+						echo form_input(array('name'=>'passenger','class'=>'form-control','id'=>'passenger','placeholder'=>'Passenger','value'=>'')); ?>
+						</div>
+					</fieldset>
+				</div>
+				<div class="booking-source">
+					<fieldset class="body-border">
+					<legend class="body-head">Booking Informations</legend>
+
+					</fieldset>
+				</div>
+			</div>
+			<div class="inner-second-column-trip-booking div-with-50-percent-width-with-margin-10">
+				<div class="booking-source">
+					<fieldset class="body-border">
+					<legend class="body-head">Booking Source</legend>
+						<div class="form-group">
+						<?php $class="form-control row-source-50-percent-width-with-margin-8";
+						echo $this->form_functions->populate_dropdown('status',$user_status='',$selected='',$class,$msg="Select Source");
 						echo form_input(array('name'=>'Source','class'=>'form-control row-source-50-percent-width-with-margin-8','id'=>'description','placeholder'=>'Source','value'=>'')); ?>
 						</div>
 					</fieldset>
@@ -25,9 +52,6 @@
 
 					</fieldset>
 				</div>
-			</div>
-			<div class="inner-second-column-trip-booking div-with-50-percent-width-with-margin-10">
-				col2
 			</div>
 		</fieldset>
 	</div>
