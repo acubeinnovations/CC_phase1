81bcc5e34509a3b0cdf74e6dc58e929f1059de1b
rosyswapna <rosy.swapna@gmail.com>
2014-09-25 14:56:11 +0530

    organisation admin fa pages added

--- facnc/sync_cnc.php
+++ facnc/sync_cnc.php
@@ -18,6 +18,34 @@
 		meta_forward('admin/company_preferences.php');
 		
 	}
+	elseif(isset($_GET['DisplaySetup'])){
+		meta_forward('admin/display_prefs.php');
+		
+	}
+	elseif(isset($_GET['FormSetup'])){
+		meta_forward('admin/forms_setup.php');
+		
+	}
+	elseif(isset($_GET['PaymentTerms'])){
+		meta_forward('admin/payment_terms.php');
+		
+	}
+	elseif(isset($_GET['FiscalYear'])){
+		meta_forward('admin/fiscalyears.php');
+		
+	}
+	elseif(isset($_GET['SystemGl'])){
+		meta_forward('admin/gl_setup.php');
+		
+	}
+	elseif(isset($_GET['VoidTransaction'])){
+		meta_forward('admin/void_transaction.php');
+		
+	}
+	elseif(isset($_GET['Backup'])){
+		meta_forward('admin/backups.php');
+		
+	}
 
 	
 	
