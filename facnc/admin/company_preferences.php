1fdec7a734abea2a4c8261f2bda71a1f493ea21d
rosyswapna <rosy.swapna@gmail.com>
2014-09-30 10:20:22 +0530

    payment terms in company setup

--- facnc/admin/company_preferences.php
+++ facnc/admin/company_preferences.php
@@ -110,7 +110,7 @@ if (isset($_POST['update']) && $_POST['update'] != "")
 				'no_item_list' => 0, 'no_customer_list' => 0, 
 				'no_supplier_list' =>0, 'base_sales', 
 				'time_zone' => 0, 'add_pct', 'round_to', 'login_tout', 'auto_curr_reval',
-				'bcc_email'))
+				'bcc_email','default_payment_terms'))
 		);
 
 		$_SESSION['wa_current_user']->timeout = $_POST['login_tout'];
@@ -155,6 +155,7 @@ $_POST['no_item_list']  = $myrow["no_item_list"];
 $_POST['no_customer_list']  = $myrow["no_customer_list"];
 $_POST['no_supplier_list']  = $myrow["no_supplier_list"];
 $_POST['curr_default']  = $myrow["curr_default"];
+$_POST['default_payment_terms']  = $myrow["default_payment_terms"];
 $_POST['f_year']  = $myrow["f_year"];
 $_POST['time_zone']  = $myrow["time_zone"];
 $_POST['version_id']  = $myrow["version_id"];
@@ -185,6 +186,7 @@ text_row_ex(_("Official Company Number:"), 'coy_no', 25);
 text_row_ex(_("GSTNo:"), 'gst_no', 25);
 
 currencies_list_row(_("Home Currency:"), 'curr_default', $_POST['curr_default']);
+payment_type_list_row(_("Payment Terms:"), 'default_payment_terms', $_POST['default_payment_terms']);
 fiscalyears_list_row(_("Fiscal Year:"), 'f_year', $_POST['f_year']);
 text_row_ex(_("Tax Periods:"), 'tax_prd', 10, 10, '', null, null, _('Months.'));
 text_row_ex(_("Tax Last Period:"), 'tax_last', 10, 10, '', null, null, _('Months back.'));
