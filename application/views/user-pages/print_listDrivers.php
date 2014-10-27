878b03c6b69738bc02453cc72396271e09e42384
Divya <divyamanoj92@gmail.com>
2014-10-27 15:07:42 +0530

    updations

--- application/views/user-pages/print_listDrivers.php
+++ application/views/user-pages/print_listDrivers.php
@@ -8,15 +8,17 @@ header("Pragma: public");
 	<table class="table table-hover table-bordered">
 	<tbody>
 		<tr>
-		<td class="excel">Name</td>
-		<td class="excel">Contact Info</td>
-		<td class="excel">Address</td>
+		<th>Name</th>
+		<th>Contact Info</th>
+		<th>Address</th>
+		<th>District</th>
 		</tr>
 		<?php foreach ($values as $val): ?>
 		<tr>
 		<td><?php echo $val['name']; ?></td>
 		<td><?php echo $val['phone'].br().$val['mobile']; ?></td>
-		<td><?php echo $val['present_address'].br().$val['district'];?></td>
+		<td><?php echo $val['present_address'];?></td>
+		<td><?php echo $val['district'];?></td>
 		</tr>
 		<?php endforeach;?>
 	</tbody>
