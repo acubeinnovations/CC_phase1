f1be043f91375ae8a53aabbc89ea9930166d76c7
Divya <divyamanoj92@gmail.com>
2014-11-21 15:08:46 +0530

    print trip booking list updations

--- application/controllers/download_xl.php
+++ application/controllers/download_xl.php
@@ -300,7 +300,7 @@ FROM vehicles V where V.organisation_id = '.$this->session->userdata('organisati
 			
 			$data['trips']=$this->print_model->all_details($qry);
 			if(empty($data['trips']) || $data['trips']==false){
-				$data['result']="No Results Found !";
+				$data['msg']="No Results Found !";
 			}
 			$data['status_class']=array(TRIP_STATUS_PENDING=>'label-warning',TRIP_STATUS_CONFIRMED=>'label-success',TRIP_STATUS_CANCELLED=>'label-danger',TRIP_STATUS_CUSTOMER_CANCELLED=>'label-danger',TRIP_STATUS_ON_TRIP=>'label-primary',TRIP_STATUS_TRIP_COMPLETED=>'label-success',TRIP_STATUS_TRIP_PAYED=>'label-info',TRIP_STATUS_TRIP_BILLED=>'label-success');
 			$data['trip_statuses']=$this->user_model->getArray('trip_statuses'); 
