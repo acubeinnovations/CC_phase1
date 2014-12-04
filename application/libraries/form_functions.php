91cdc72e89f483b4324b2b8d811bcbbc06f1f335
Divya <divyamanoj92@gmail.com>
2014-12-04 16:16:48 +0530

    attribute modification in form functions

--- application/libraries/form_functions.php
+++ application/libraries/form_functions.php
@@ -1,8 +1,12 @@
 <?php 
 class Form_functions{
-function populate_dropdown($name = '', $options = array(), $selected = array(),$class='',$id='',$msg='select'){
+function populate_dropdown($name = '', $options = array(), $selected = array(),$class='',$id='',$msg='select',$attr=array()){
 $CI = & get_instance();
-$form = '<select name="'.$name.'" class="'.$class.'" id="'.$id.'"/>';
+$attr_str='';
+foreach($attr as $k=>$v){
+$attr_str.= ' '.$k.'="'.$v.'"';
+}
+$form = '<select'.$attr_str.' name="'.$name.'" class="'.$class.'" id="'.$id.'"/>';
 if($selected==''){
 $form.='<option value="-1" selected="selected" >--'.$msg.'--</option></br>';
 }
